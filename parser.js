8f042d14b832c9e7088c3a960621efe89ac0d3b5
NohponeX <nohponex@gmail.com>
2013-02-25 22:14:50 +0200

    XOR Operator Added

--- parser.js
+++ parser.js
@@ -1,27 +1,28 @@
 /*
-	Boole Function Parser 
-	Author: NohponeX (xenofons@auth.gr)
-	Version: 1.01
-	Date: 7/11/2011
-*/
+ Boole Function Parser
+ Author: NohponeX (xenofons@auth.gr)
+ Version: 1.01
+ Date: 7/11/2011
+ */
 
+var current = 0;
 
-
-var current=0;
-
-function keyPressedEvt(){
+function keyPressedEvt() {
 	var e = window.event;
-	if (e.keyCode) x = e.keyCode;
-   else if (e.which) x = e.which;
-	if(x==13){//Enter key pressed
-     eval();
-   }
+	if (e.keyCode)
+		x = e.keyCode;
+	else if (e.which)
+		x = e.which;
+	if (x == 13) {//Enter key pressed
+		eval();
+	}
 }
-function eval(){
+
+function eval() {
 	parser(document.getElementById("input_box").value);
 }
 
-Array.prototype.contains = function (element) {
+Array.prototype.contains = function(element) {
 	for (var i = 0; i < this.length; i++) {
 		if (this[i] == element) {
 			return true;
@@ -29,16 +30,17 @@ Array.prototype.contains = function (element) {
 	}
 	return false;
 }
-function trace(str ){
+function trace(str) {
 	var result = document.getElementById("results");
-	
-	result.innerHTML  +="<p>"+str+"</p>";
+
+	result.innerHTML += "<p>" + str + "</p>";
 }
 
-function getLogicBit(value,position){
-	return  ((value & (1 << position)) != 0);
+function getLogicBit(value, position) {
+	return ((value & (1 << position)) != 0);
 }
-function variableIndex(variables,variable){
+
+function variableIndex(variables, variable) {
 	for (var i = 0; i < variables.length; i++) {
 		if (variables[i] == variable) {
 			return i;
@@ -47,80 +49,92 @@ function variableIndex(variables,variable){
 	return -1;
 }
 
-function isValid(c){
-	if( c == '!' || c == '+' || c == '(' || c == ')'  ) return true;
-	if( c>= 'a' && c<='z') return true;
-	if( c>= 'A' && c<='Z') return true;
+function isValid(c) {
+	if (c == '!' || c == '+' || c == '^' || c == '(' || c == ')')
+		return true;
+	if (c >= 'a' && c <= 'z')
+		return true;
+	if (c >= 'A' && c <= 'Z')
+		return true;
 	return false;
 }
 
-function parser(input){
+function parser(input) {
 	var result = document.getElementById("results");
-	
+
 	//remove whitespaces
-	var input = input.replace(/\s+/g,"");
-	
+	var input = input.replace(/\s+/g, "");
+
 	//Input Validation
-	
-	for(var i=0; i< input.length; i++){
-	
-		if(!isValid(input.charAt(i))){
-			result.innerHTML = "ERROR!\n ilegal character found at position " + i  + "["  + input.charAt(i) + "]" +" !";
-			
+
+	for (var i = 0; i < input.length; i++) {
+
+		if (!isValid(input.charAt(i))) {
+			result.innerHTML = "ERROR!\n ilegal character found at position " + i + "[" + input.charAt(i) + "]" + " !";
+
 			return;
 		}
 	}
-	
+
 	/* Find Variables */
-	
+
 	var variables = new Array();
-	
-	for(var i=0 ; i < input.length ; i++){
+
+	for (var i = 0; i < input.length; i++) {
 		var c = input.charAt(i);
-		if( c != '!' && c != '+' && c != '(' &&  c != ')' ){
-			if(!variables.contains(c)) variables.push(c);
+		if (c != '!' && c != '+' && c != '(' && c != ')' && c != '^') {
+			if (!variables.contains(c))
+				variables.push(c);
 		}
 	}
 	variables.sort();
-	
+
 	//Create truth table
-	var variations = Math.pow(2,variables.length);
+	var variations = Math.pow(2, variables.length);
 
 	var TruthTableValues = new Array(variations);
 	var TruthTableResult = new Array(variations);
-	
-	var Index=0;
-	for(var i=0; i< variations ; i++){
-		TruthTableValues[i]= new Array(variables.length);
-		
+
+	var Index = 0;
+	for (var i = 0; i < variations; i++) {
+		TruthTableValues[i] = new Array(variables.length);
+
 		TruthTableResult[i] = false;
-		
-		for(var v=0; v< variables.length;v++){
-			TruthTableValues[i][v] = getLogicBit(Index, variables.length - v - 1 );
+
+		for (var v = 0; v < variables.length; v++) {
+			TruthTableValues[i][v] = getLogicBit(Index, variables.length - v - 1);
 		}
-		
+
 		Index++;
 	}
-	
+
 	//Set values
 	var lastNot = false;
-    var cvalue = true;
-	for(var i=0; i< variations ;i++){
-		current=0;
+	var cvalue = true;
+	for (var i = 0; i < variations; i++) {
+		current = 0;
 		lastNot = false;
 		cvalue = true;
-		for(;;){
+		for ( ; ; ) {
 			var c = input.charAt(current);
-			if( c == '!' ){
+			if (c == '!') {
 				lastNot = !lastNot;
-			}else if( c == '+'){
-				cvalue |= evaluate(input,TruthTableValues[i],variables);
-			}else if( c == '('){
-				cvalue &= (lastNot ? !evaluate(input,TruthTableValues[i],variables)  : evaluate(input,TruthTableValues[i],variables));
-				if(lastNot) lastNot = false;
-			}else{
-				cvalue &= (lastNot ? !TruthTableValues[i][ variableIndex(variables,c)] : TruthTableValues[i][ variableIndex(variables,c)]);
-				if(lastNot) lastNot = false;
+			} else if (c == '+') {
+				cvalue |= evaluate(input, TruthTableValues[i], variables);
+			} else if (c == '^') { //XOR 
+				var temp = evaluate(input, TruthTableValues[i], variables);
+				console.log(temp);
+				console.log(cvalue);
+				cvalue = (!cvalue && temp || cvalue && !temp );
+
+			} else if (c == '(') {
+				cvalue &= ( lastNot ? !evaluate(input, TruthTableValues[i], variables) : evaluate(input, TruthTableValues[i], variables));
+				if (lastNot)
+					lastNot = false;
+			} else {
+				cvalue &= ( lastNot ? !TruthTableValues[i][ variableIndex(variables, c)] : TruthTableValues[i][ variableIndex(variables, c)]);
+				if (lastNot)
+					lastNot = false;
 			}
 			current++;
 			if (current >= input.length) {
@@ -129,50 +143,57 @@ function parser(input){
 			}
 		}
 	}
-	
+
 	var out = "";
 	//Output
 	out += "<table><tr>";
-	for(var i=0; i<variables.length;i++){
+	for (var i = 0; i < variables.length; i++) {
 		out += "<th>" + variables[i] + "</th>";
 	}
 	out += "<th>&#402;</th></tr>";
-	
-	for(var i=0; i< variations ; i++){
-		var cclass = (i%2==0? "lineA" : "lineB" );
-		out  +="<tr class=\""+ cclass + "\">";
-		for(var v=0; v< variables.length;v++){
-			out  += "<td>" + (TruthTableValues[i][v] == true ? 1 : 0 )+ "</td>";
+
+	for (var i = 0; i < variations; i++) {
+		var cclass = (i % 2 == 0 ? "lineA" : "lineB" );
+		out += "<tr class=\"" + cclass + "\">";
+		for (var v = 0; v < variables.length; v++) {
+			out += "<td>" + (TruthTableValues[i][v] == true ? 1 : 0 ) + "</td>";
 		}
 		out += "<td class=\"result\">" + TruthTableResult[i] + "</td></tr>";
 	}
-	
-	out  +="</table>";
-	
+
+	out += "</table>";
+
 	result.innerHTML = out;
 }
-	
-function evaluate(input,variation,variables){
+
+function evaluate(input, variation, variables) {
 	var cvalue = true;
 	var lastNot = false;
-	for(;;){
+	for ( ; ; ) {
 		current++;
 		if (current >= input.length) {
 			return cvalue;
 		}
 		var c = input.charAt(current);
-		if( c == '!' ){
+		if (c == '!') {
 			lastNot = !lastNot;
-		}else if( c == '+'){
-			cvalue |= evaluate(input,variation,variables);
-		}else if( c == '('){
-			cvalue &= (lastNot ? !evaluate(input,variation,variables)  : evaluate(input,variation,variables));
-			if(lastNot) lastNot =false;
-		}else if( c == ')'){
+		} else if (c == '+') {//OR
+			cvalue |= evaluate(input, variation, variables);
+		} else if (c == '^') {//XOR
+			var temp = evaluate(input, variation, variables);
+			console.log(temp);
+			console.log(cvalue);
+			cvalue = (!cvalue && temp || cvalue && !temp );
+		} else if (c == '(') { //AND
+			cvalue &= ( lastNot ? !evaluate(input, variation, variables) : evaluate(input, variation, variables));
+			if (lastNot)
+				lastNot = false;
+		} else if (c == ')') {
 			return cvalue;
-		}else{
-			cvalue &= (lastNot ? !variation[variableIndex(variables,c)] : variation[ variableIndex(variables,c)]);
-			if(lastNot) lastNot =false;
+		} else { //AND
+			cvalue &= ( lastNot ? !variation[variableIndex(variables, c)] : variation[ variableIndex(variables, c)]);
+			if (lastNot)
+				lastNot = false;
 		}
 	}
 	return cvalue;
